11b304c5468e141ef56c8ceb1eb93f6589eda6a8
Utsav Karki <Utsavkarki244@gmail.com>
2021-11-30 11:57:20 +0545

    Refractor

--- autoinsta.py
+++ autoinsta.py
@@ -11,5 +11,3 @@ sleep(20)
 # users = session.target_list("username3.txt")
 session.set_comments(users, amount=3, randomize=False)
 session.set_action_delays(enabled=True, comment=5.2, randomize=True, random_range_from=70, random_range_to=140)
-
-
